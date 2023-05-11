9e81395a434c648022aa92071c5defa152906c08
JavierP <javierjosepp2000@gmail.com>
2023-05-11 13:06:05 -0400

    Creando modelos para la base de datos y montando la aplicación

---
+++ app/biblioteca_SC/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+# def hola(request):
+#     title = 'SERVICIO COMUNITARIO'
+#     return render(request,'index.html', {
+#         'title' : title
+#     })
