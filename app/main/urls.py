348d3c5e7d8913237d38467666f2b01ce697087c
isaias <algerninjaxdd@hotmail.com>
2023-06-06 12:20:26 -0400

    buscador

--- app/main/urls.py
+++ app/main/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from .views import IndexView, DocumentDetailView, SearchResultsView
+from .views import IndexView, DocumentDetailView
 
 app_name = "main"
 urlpatterns = [
     path("", IndexView.as_view(), name="home"),
-    # path("view/", DocumentDetailView.as_view(), name="document"),
     path("view/<str:id>/", DocumentDetailView.as_view(), name="document"),
-    path('search/<str:q>/', SearchResultsView.as_view(), name='search_results'),
 ]
--- app/main/views.py
+++ app/main/views.py
@@ -12,6 +12,7 @@ from biblioteca_SC.utils.driver_connection import get_id_from_url
 from authentication.forms import LoginForm
 from .forms import FilterForm
 
+
 # Create your views here.
 class IndexView(View):
     form_login = LoginForm
@@ -25,22 +26,33 @@ class IndexView(View):
     def get_projects(self, request):
         proyectos_list = self.model.objects.get_queryset().order_by("periodo")
         paginator = Paginator(proyectos_list, self.row_for_page)
-        pagina = request.GET.get("pagina")
+        pagina = request.GET.get("pagina    ")
         return paginator.get_page(pagina)
-    
-
 
     def get(self, request, *args, **kwargs):
         context = {}
+
+        print(
+            "➡ request :",
+        )
+        print("➡ args :", args)
+        print("➡ kwargs :", kwargs)
+        # if kwargs.get("q"):
+
+        proyectos = self.get_projects(request)
+        context["proyectos"] = proyectos
+
+        if_query = request.GET.get("q")
+        if if_query:
+            context["proyectos"] = Project_SC.objects.filter(titulo__icontains=if_query)
+
         if request.GET.get("next"):
             context["showFormLogin"] = "show"
 
         form_filter1 = self.form_filter1()
         form_login = self.form_login()
-        proyectos = self.get_projects(request)
 
         context["form_login"] = form_login
-        context["proyectos"] = proyectos
         context["form_filter1"] = form_filter1
         return render(request, self.template_name, context=context)
 
@@ -64,13 +76,10 @@ class IndexView(View):
             if user:
                 login(request, user)
                 return redirect("main:home")
-            
+
         context["proyectos"] = proyectos
         return render(request, self.template_name, context=context)
         # return redirect("main:home")
-
-    
-
 
     # def post(self, request, *args, **kwargs):
     #     return HttpResponse('POST request!')
@@ -93,21 +102,4 @@ class DocumentDetailView(View):
             file_url = None
 
         context = {"form_login": form_login, "proyecto": proyecto, "file_url": file_url}
-        return render(request, self.template_name, context=context)
-    
-class SearchResultsView(View):
-    template_name = "index.main.html"
-    model = Project_SC
-
-    def get(self, request, *args, **kwargs):
-
-        query = kwargs["q"]
-
-        filter_proyectos = Project_SC.objects.filter(titulo__icontains=query)
-
-        context = {'filter_proyectos':filter_proyectos}
-        # query = self.request.GET.get("texto")
-
-
-        
         return render(request, self.template_name, context=context)
